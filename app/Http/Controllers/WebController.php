39014b08b6aa7e7baeba9640da2bb2cd4f636d46
Guillermo Pacherres <gerencia@bertha.pe>
2024-05-23 13:11:28 -0400

    sa

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -8,7 +8,8 @@ class WebController extends Controller
 {
     public function indexView(){
 
-        return view('Index.home');
+        //return view('Index.home');
+        return redirect()->to('https://wa.me/18134010513?text=Hi%20Phillip%21%20I%27m%20interested%20on%20create%20a%20*ISYML*%20profile');
     }
     public function viewRachelP(){
 
